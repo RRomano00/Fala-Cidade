178d3dacccbff1de4102608ea08a5fc1a3101218
MiguelMartns <martinsmiguel659@gmail.com>
2025-06-04 22:05:23 -0300

    chore: criacao dos arquivos bases do projeto

---
+++ Projetos/Front-end/src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { SignInComponent } from './views/account/sign-in/sign-in.component';
+import { SignUpComponent } from './views/account/sign-up/sign-up.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { MainComponent } from './views/app/main/main.component';
+import { MyProfileComponent } from './views/account/my-profile/my-profile.component';
+
+
+export const routes: Routes = [
+    {
+        path: 'account/sign-in',
+        component: SignInComponent
+    },
+    {
+        path: 'account/sign-up',
+        component: SignUpComponent
+    },
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            {
+                path: 'account/my-profile',
+                component: MyProfileComponent
+            },
+            {
+                path: 'main',
+                component: MainComponent
+            },
+            {
+            path: '**',
+            component: NotFoundComponent
+        },
+            ],
+        
+    },
+];
---
+++ Projetos/Front-end/src/app/views/app/main/main.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{FormsModule} from '@angular/forms';
+import{MatButtonModule} from '@angular/material/button';
+import{MatSidenavModule} from '@angular/material/sidenav';
+import{MatMenuModule} from '@angular/material/menu';
+import{MatIconModule} from '@angular/material/icon';
+import{MatListModule} from '@angular/material/list';
+import{MatTooltipModule} from '@angular/material/tooltip';
+import{MatExpansionModule} from '@angular/material/expansion';
+import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import * as fontawesome from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    FontAwesomeModule,
+
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+    faFooterIcon = fontawesome.faBacteria;
+
+
+}
