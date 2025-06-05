6f2c83a552f02957f08a771563ffe89caca6766e
Miguel <martinsmiguel659@gmail.com>
2025-06-05 00:45:51 -0300

    feat: adicao e criacao de alguns componentes da tela denuncia

---
+++ Projetos/Front-end/src/app/views/denuncia/denuncia.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DenunciaComponent } from './denuncia.component';
+
+describe('DenunciaComponent', () => {
+  let component: DenunciaComponent;
+  let fixture: ComponentFixture<DenunciaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DenunciaComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DenunciaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
