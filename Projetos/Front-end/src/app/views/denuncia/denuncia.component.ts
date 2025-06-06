45cd7b110cd8e3618d83659544a8447396caaf22
Miguel <martinsmiguel659@gmail.com>
2025-06-06 18:14:14 -0300

    feat: adicao da funcao de selecionar denuncia anonima

--- Projetos/Front-end/src/app/views/denuncia/denuncia.component.ts
+++ Projetos/Front-end/src/app/views/denuncia/denuncia.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{FormsModule} from '@angular/forms';
+import{MatButtonModule} from '@angular/material/button';
+import{MatSidenavModule} from '@angular/material/sidenav';
+import{MatMenuModule} from '@angular/material/menu';
+import{MatIconModule} from '@angular/material/icon';
+import{MatListModule} from '@angular/material/list';
+import{MatTooltipModule} from '@angular/material/tooltip';
+import{MatExpansionModule} from '@angular/material/expansion';
+import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-denuncia',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    FormsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './denuncia.component.html',
   styleUrl: './denuncia.component.css'
 })
