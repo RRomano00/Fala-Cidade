5cbbd7d74c96e4623b15e91a50e078e7477024e3
Furlan07 <lucasfiorrigif@gmail.com>
2025-06-09 20:00:13 -0300

    feat: Criacao do modelo de usuario

---
+++ Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/domain/UserModel.java
@@ -0,0 +1,24 @@
+package br.com.faitec.fala_cidade.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+
+    private int id;
+    private String email;
+    private String password;
+    private String fullname;
+    private UserRole role;
+
+    public enum UserRole{
+        ADMINISTRATOR,
+        USER
+    }
+
+}
