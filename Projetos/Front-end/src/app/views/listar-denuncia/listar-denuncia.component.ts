636d6f58792bac601e0e34f168e66c3e293562ce
Miguel <martinsmiguel659@gmail.com>
2025-06-09 12:22:19 -0300

    feat: implementacao do listar denuncia

---
+++ Projetos/Front-end/src/app/views/listar-denuncia/listar-denuncia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-denuncia',
+  imports: [],
+  templateUrl: './listar-denuncia.component.html',
+  styleUrl: './listar-denuncia.component.css'
+})
+export class ListarDenunciaComponent {
+
+}
