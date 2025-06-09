c4caa7e8a0fb5fecdc7010c9037d907fc3758f00
Miguel <martinsmiguel659@gmail.com>
2025-06-09 15:55:27 -0300

    feat: adicao das telas de editar perfil

--- Projetos/Front-end/src/app/views/account/my-profile/my-profile.component.ts
+++ Projetos/Front-end/src/app/views/account/my-profile/my-profile.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css'
 })
