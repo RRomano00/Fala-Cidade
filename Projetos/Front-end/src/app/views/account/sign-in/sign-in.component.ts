41fe7d7c9bbe17900d3fb1bbeca718175000e845
Miguel <martinsmiguel659@gmail.com>
2025-06-10 11:44:37 -0300

    feat: import RouterModule em sign-in

--- Projetos/Front-end/src/app/views/account/sign-in/sign-in.component.ts
+++ Projetos/Front-end/src/app/views/account/sign-in/sign-in.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import{MatToolbarModule} from '@angular/material/toolbar';
-import{FormsModule, ReactiveFormsModule} from '@angular/forms';
-import{MatButtonModule} from '@angular/material/button';
-import{MatSidenavModule} from '@angular/material/sidenav';
-import{MatMenuModule} from '@angular/material/menu';
-import{MatIconModule} from '@angular/material/icon';
-import{MatListModule} from '@angular/material/list';
-import{MatTooltipModule} from '@angular/material/tooltip';
-import{MatExpansionModule} from '@angular/material/expansion';
-import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import{MatInputModule} from '@angular/material/input';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
@@ -29,10 +29,11 @@ import{MatInputModule} from '@angular/material/input';
     FontAwesomeModule,
     ReactiveFormsModule,
     MatInputModule,
+    RouterModule
   ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-    
+
 }
