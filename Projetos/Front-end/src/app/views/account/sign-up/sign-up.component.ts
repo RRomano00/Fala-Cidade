45e11ebda1cdf082d5fd04ebf3e38824e35bdfc8
Miguel <martinsmiguel659@gmail.com>
2025-06-10 11:44:45 -0300

    feat: import RouterModule em sign-up

--- Projetos/Front-end/src/app/views/account/sign-up/sign-up.component.ts
+++ Projetos/Front-end/src/app/views/account/sign-up/sign-up.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import{MatToolbarModule} from '@angular/material/toolbar';
-import{FormsModule, ReactiveFormsModule} from '@angular/forms';
-import{MatButtonModule} from '@angular/material/button';
-import{MatSidenavModule} from '@angular/material/sidenav';
-import{MatMenuModule} from '@angular/material/menu';
-import{MatIconModule} from '@angular/material/icon';
-import{MatListModule} from '@angular/material/list';
-import{MatTooltipModule} from '@angular/material/tooltip';
-import{MatExpansionModule} from '@angular/material/expansion';
-import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import{MatInputModule} from '@angular/material/input';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-up',
@@ -28,6 +28,7 @@ import{MatInputModule} from '@angular/material/input';
     FontAwesomeModule,
     ReactiveFormsModule,
     MatInputModule,
+    RouterModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
