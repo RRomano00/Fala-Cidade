eee4576023c07c26681755743d8c088327515234
Miguel <martinsmiguel659@gmail.com>
2025-06-10 11:29:33 -0300

    feat: insercao do RouterModule

--- Projetos/Front-end/src/app/views/app/main/main.component.ts
+++ Projetos/Front-end/src/app/views/app/main/main.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import{MatToolbarModule} from '@angular/material/toolbar';
-import{FormsModule} from '@angular/forms';
-import{MatButtonModule} from '@angular/material/button';
-import{MatSidenavModule} from '@angular/material/sidenav';
-import{MatMenuModule} from '@angular/material/menu';
-import{MatIconModule} from '@angular/material/icon';
-import{MatListModule} from '@angular/material/list';
-import{MatTooltipModule} from '@angular/material/tooltip';
-import{MatExpansionModule} from '@angular/material/expansion';
-import{FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import * as fontawesome from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -26,10 +26,11 @@ import * as fontawesome from '@fortawesome/free-solid-svg-icons';
     MatTooltipModule,
     MatExpansionModule,
     FontAwesomeModule,
+    RouterModule
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-    faFooterIcon = fontawesome.faHeart;
+  faFooterIcon = fontawesome.faHeart;
 }
