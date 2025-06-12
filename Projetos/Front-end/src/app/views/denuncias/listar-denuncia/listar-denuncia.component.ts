eb877645b740b949aeae1773799f1a592ce30ef6
Miguel <martinsmiguel659@gmail.com>
2025-06-12 18:47:27 -0300

    refactor: alteracao do nome do componente criar denuncia

--- Projetos/Front-end/src/app/views/denuncias/listar-denuncia/listar-denuncia.component.ts
+++ Projetos/Front-end/src/app/views/denuncias/listar-denuncia/listar-denuncia.component.ts
@@ -2,11 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ReadDenunciaService } from '../../../services/read-denuncia.service';
 import { Denuncia } from '../../../domain/model/denuncia';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-listar-denuncia',
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './listar-denuncia.component.html',
   styleUrl: './listar-denuncia.component.css'
