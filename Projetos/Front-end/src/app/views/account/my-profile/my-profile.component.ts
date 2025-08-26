90dd339ce6bc27394854510f433612d3a6198128
Miguel <martinsmiguel659@gmail.com>
2025-08-26 10:29:49 -0300

    feat: carregando campo com nome e email do usuario logado

--- Projetos/Front-end/src/app/views/account/my-profile/my-profile.component.ts
+++ Projetos/Front-end/src/app/views/account/my-profile/my-profile.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { UserReadService } from '../../../services/user/user-read.service';
+import { AuthenticationService } from '../../../services/security/authentication.service';
+import { FormsModule } from "@angular/forms";
+import { User } from '../../../domain/model/user';
 
 @Component({
   selector: 'app-my-profile',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    FormsModule
   ],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css'
 })
 export class MyProfileComponent {
 
+  user: User = { fullname: '', email: '', password: '', role: '' };
+
+  ngOnInit(): void {
+    const fullname = localStorage.getItem('fullname') || '';
+    const email = localStorage.getItem('email') || '';
+
+    this.user.fullname = fullname;
+    this.user.email = email;
+  }
 }
