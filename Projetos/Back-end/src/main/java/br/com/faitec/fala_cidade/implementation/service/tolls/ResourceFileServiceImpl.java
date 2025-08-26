a31cb632dc137824d1fe9f827a12e350b1de2bba
Furlan07 <lucasfiorrigif@gmail.com>
2025-08-26 10:33:05 -0300

    feat: criacao da interface e metodo de leitura de path para as sql

---
+++ Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/implementation/service/tolls/ResourceFileServiceImpl.java
@@ -0,0 +1,35 @@
+package br.com.faitec.fala_cidade.implementation.service.tolls;
+
+import br.com.faitec.fala_cidade.port.service.tools.ResourceFileService;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@Service
+public class ResourceFileServiceImpl implements ResourceFileService {
+
+    @Override
+    public String read(String resourcePath) throws IOException {
+        final ClassLoader classLoader = ResourceFileServiceImpl.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(resourcePath);
+
+        if (inputStream == null){
+            throw new RuntimeException("Arquivo não encontrado");
+        }
+
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String content = "";
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null){
+            System.out.println(line);
+            content += line;
+        }
+
+        return content;
+    }
+}
