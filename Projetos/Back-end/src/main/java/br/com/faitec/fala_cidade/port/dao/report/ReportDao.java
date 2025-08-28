1c30c957d94c532372160ee6e1e3e072ada4fbdc
Furlan07 <lucasfiorrigif@gmail.com>
2025-08-27 21:55:12 -0300

    refeactor: alteracao dos metodos de update para retotrnar GetReport

--- Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/port/dao/report/ReportDao.java
+++ Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/port/dao/report/ReportDao.java
@@ -1,10 +1,11 @@
 package br.com.faitec.fala_cidade.port.dao.report;
 
 import br.com.faitec.fala_cidade.domain.Report;
+import br.com.faitec.fala_cidade.domain.dto.GetReport;
 import br.com.faitec.fala_cidade.port.dao.crud.CreateDao;
 import br.com.faitec.fala_cidade.port.dao.crud.ReadDao;
 
-public interface ReportDao extends CreateDao<Report>, ReadDao<Report>{
+public interface ReportDao extends CreateDao<Report>, ReadDao<GetReport>{
     void updateReportStatusToInProgress(final int id);
 
     void updateReportStatusToConclude(final int id);
--- Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/port/service/report/ReportService.java
+++ Projetos/Back-end/src/main/java/br/com/faitec/fala_cidade/port/service/report/ReportService.java
@@ -1,10 +1,11 @@
 package br.com.faitec.fala_cidade.port.service.report;
 
 import br.com.faitec.fala_cidade.domain.Report;
+import br.com.faitec.fala_cidade.domain.dto.GetReport;
 import br.com.faitec.fala_cidade.port.service.crud.CreateService;
 import br.com.faitec.fala_cidade.port.service.crud.ReadService;
 
-public interface ReportService extends CreateService<Report>, ReadService<Report> {
+public interface ReportService extends CreateService<Report>, ReadService<GetReport> {
 
     void updateReportStatusToInProgress(final int id);
 
