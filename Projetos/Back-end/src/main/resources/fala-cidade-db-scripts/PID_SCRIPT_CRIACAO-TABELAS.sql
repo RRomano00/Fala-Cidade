bc8194d9d94beab4b818f7800fc9ec25727c6c42
Furlan07 <lucasfiorrigif@gmail.com>
2025-08-28 08:43:36 -0300

    refeactor:  alteracao do script das tabelas

--- Projetos/Back-end/src/main/resources/fala-cidade-db-scripts/PID_SCRIPT_CRIACAO-TABELAS.sql
+++ Projetos/Back-end/src/main/resources/fala-cidade-db-scripts/PID_SCRIPT_CRIACAO-TABELAS.sql
@@ -6,8 +6,6 @@ begin;
     drop table if exists classification cascade;
     drop table if exists city_employee cascade;
     drop table if exists department cascade;
-    drop table if exists complainant cascade;
-    drop table if exists administrator cascade;
     drop table if exists city cascade;
     drop table if exists users cascade;
 
@@ -31,30 +29,10 @@ begin;
         unique(name, state)
     );
 
-    create table complainant(
-        id serial primary key,
-        neighborhood varchar(100) not null,
-        number varchar(10) not null,
-        street varchar(100) not null,
-        city varchar(100) not null,
-        cep varchar(9),
-        city_id integer not null references city(id),
-        users_id integer not null references users(id) on delete cascade,
-        unique(users_id)
-    );
-
     create table department(
         id serial primary key,
         name varchar(50) not null,
         city_id integer not null references city(id) on delete cascade
-    );
-
-    create table city_employee(
-        id serial primary key,
-        city_id integer not null references city(id),
-        department_id integer not null references department(id),
-        users_id integer not null references users(id) on delete cascade,
-        unique(users_id)
     );
 
     create table report(
@@ -81,21 +59,14 @@ begin;
             'OUTROS_PROBLEMAS'
         )),
         department_id integer references department(id),
-        city_employee_id integer references city_employee(id),
-        complainant_id integer references complainant(id),
-        unique (complainant_id, creation_date)
+        users_id integer references users(id),
+        unique (users_id, creation_date)
     );
 
     create table media(
         id serial primary key,
         file_path text not null,
         report_id integer not null references report(id) on delete cascade
-    );
-
-    create table administrator(
-        id serial primary key,
-        users_id integer not null references users(id) on delete cascade,
-        unique(users_id)
     );
 
 commit;
